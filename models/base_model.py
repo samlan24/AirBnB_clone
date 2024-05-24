ba1351747c57e25a3978ec598c63f1f30f0eda27
samlan24 <muchemionline@gmail.com>
2024-05-24 09:12:57 -0700

    adding base model class

---
+++ models/base_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""The base class for the project"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """class initialization"""
+
+    def __int__(self):
+        """initializing public instance attributes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """updates public instance updated_at"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns dictionary with key/values"""
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        return my_dict
