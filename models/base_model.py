6fce7ad491ea7f97fc34fdc686f06740d2146a5e
samlan24 <muchemionline@gmail.com>
2024-05-29 06:25:39 -0700

    updating the base model

--- models/base_model.py
+++ models/base_model.py
@@ -15,11 +15,17 @@ class BaseModel:
             **kwargs: key-value dictionary
 
         """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
 
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
